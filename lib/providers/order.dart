876cc95373f93933db6dc2b8529ef6f9d8105537
Indrajith V <indrajithvgp@yahoo.com>
2021-10-18 11:33:28 +0530

    orders fetch and add

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 // import 'package:shop_app/widgets/cart_item.dart';
 import './cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrderItem {
   final String id;
@@ -22,14 +25,70 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert( 
+  // void addOrder(List<CartItem> cartProducts, double total) {
+  //   _orders.insert(
+  //       0,
+  //       OrderItem(
+  //         id: DateTime.now().toString(),
+  //         dateTime: DateTime.now(),
+  //         amount: total,
+  //         products: cartProducts
+  //       ));
+  // }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url =
+        "https://flutter-app-b86f6-default-rtdb.firebaseio.com/orders.json";
+    final timeStamp = DateTime.now();
+    final response = await http.post(Uri.parse(url),
+        body: json.encode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          "productId": cartProducts
+              .map((e) => ({
+                    'id': e.id,
+                    'quantity': e.quantity,
+                    "price": e.price,
+                    "title": e.title,
+                  }))
+              .toList()
+        }));
+    _orders.insert(
         0,
         OrderItem(
-          id: DateTime.now().toString(),
-          dateTime: DateTime.now(),
-          amount: total,
-          products: cartProducts
-        ));
+            id: json.decode(response.body)['name'],
+            dateTime: timeStamp,
+            amount: total,
+            products: cartProducts));
+    notifyListeners();
+  }
+
+  Future<void> fetchOrders() async {
+    final url =
+        "https://flutter-app-b86f6-default-rtdb.firebaseio.com/orders.json";
+
+    final response = await http.get(Uri.parse(url));
+    final List<OrderItem> loadedOrders = [];
+    final extractedResponse =
+        json.decode(response.body) as Map<String, dynamic>;
+    if (extractedResponse == null) {
+      return;
+    }
+    extractedResponse.forEach((key, value) {
+      loadedOrders.add(OrderItem(
+          id: key,
+          amount: value["amount"],
+          dateTime: DateTime.parse(value['dateTime']),
+          products: (value['products'] as List<dynamic>)
+              .map((item) => CartItem(
+                  id: item.id,
+                  title: item.title,
+                  price: item.price,
+                  quantity: item.quantity))
+              .toList()));
+    });
+
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
   }
 }
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -4,18 +4,40 @@ import 'package:shop_app/providers/order.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart' as screen;
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   static const routeName = "/orders";
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 0)).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context).fetchOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
     return Scaffold(
         appBar: AppBar(title: Text("Your Orders")),
         drawer: AppDrawer(),
-        body: ListView.builder(
-            itemCount: orderData.orders.length,
-            itemBuilder: (ctx, i) => screen.OrderItem(
-                  order: orderData.orders[i],
-                )));
+        body: _isLoading
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, i) => screen.OrderItem(
+                      order: orderData.orders[i],
+                    )));
   }
 }
